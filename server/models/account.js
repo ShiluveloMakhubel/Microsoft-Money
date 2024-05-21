4fca21cf44a3b43a9c0a7b70bf8f8924de0ded84
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-05-21 21:57:05 +0200

    Create account.js

---
+++ server/models/account.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+
+  
+  name: {
+    type: String
+  
+  },
+  balance: {
+    type: Number
+   
+  },
+  type: {
+    type: String,
+   
+  },
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+module.exports = Account;
