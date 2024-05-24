6a4defaa21e189e2c8bf86d4786c373dfc1455ed
ShiluveloMakhubel <117033636+ShiluveloMakhubel@users.noreply.github.com>
2024-05-24 20:22:16 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,14 @@
-// jest.setup.js
-import '@testing-library/jest-dom';
+require('dotenv').config();
+
+module.exports = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transform: {
+      '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+    testTimeout: 30000,  // 30 seconds
+  };
+  
